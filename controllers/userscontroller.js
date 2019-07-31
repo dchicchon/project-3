47501d40776870d2c6a3b7e4b95e55b7f375213f
matkuh <matkuh@gmail.com>
2019-07-31 15:22:14 -0700

    create usercontroller, start adding requires and keys for s3 bucket

---
+++ controllers/userscontroller.js
@@ -0,0 +1,12 @@
+const db = require("../models")
+
+const AWS = require("aws-sdk");
+const uuid = require("uuid/v4");
+const s3 = new AWS.S3({
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
+
+module.exports = {
+    
+}
