b2fbce83d50cd3e3269b47d536280cc21c4fe8d9
blueDogs <danielchicchon@gmail.com>
2019-07-31 17:18:54 -0700

    add auth.js which contains the routes for handling posts and get requests that involve passport.js

--- server.js
+++ server.js
@@ -1,35 +1,41 @@
+// Set up
+require('dotenv').config();
+
+// Dependencies
 const express = require("express");
-const cookieParser = require("cookie-parser");
-const flash = require("express-flash-messages");
+// const cookieParser = require("cookie-parser");
+// const flash = require("express-flash-messages");
 const session = require("express-session");
 const passport = require("passport")
 
 const app = express();
-const routes = require("./routes")
 const PORT = process.env.PORT || 4815
 
 const db = require("./models")
+const routes = require("./routes")
 
 // Middleware
+require("./config/passport")(passport)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"))
 }
 
-// Cookie parser
-app.use(cookieParser());
-app.use(flash());
-require("./config/passport")(passport)
+// // Cookie parser
+// app.use(cookieParser());
+// app.use(flash());
 
 app.use(session({
     // Use dotenv dependency to hide secret
-    secret: "Guitar Dog",
-    resave: false,
+    key: 'user_sid',
+    secret: process.env.SESSION_SECRET,
+    resave: true,
     saveUninitialized: false,
     cookie: {
-        expires: 299999999999,
+        expires: 10800000, // 3 hrs
         httpOnly: false
     }
 }));
@@ -40,14 +46,14 @@ app.use(passport.initialize());
 // Persistent login sessions. Session expires after something or when deleted by user
 app.use(passport.session())
 
-// enable CORS so that browsers don't block requests.
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://localhost:4815");
-    res.header("Access-Control-Allow-Credentials", 'true');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
-});
+// // enable CORS so that browsers don't block requests.
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "https://localhost:4815");
+//     res.header("Access-Control-Allow-Credentials", 'true');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//     next();
+// });
 
 // Routes
 app.use(routes)
