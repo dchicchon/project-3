41e74191276069cdba7263ee9f9167c0f9c5d52f
Adrienne <iliu.adrienne@gmail.com>
2019-08-02 15:24:20 -0700

    test

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from './node_modules/react';
 
 
 
@@ -8,12 +8,12 @@ export default function NavBar(props) {
     <div>
 
       <nav>
-        <div class="nav-wrapper blue">
-          <a href="/feed" class="brand-logo">TripIt</a>
-          <ul class="right hide-on-med-and-down">
+        <div className="nav-wrapper blue">
+          <a href="/feed" className="brand-logo">TripIt</a>
+          <ul className="right hide-on-med-and-down">
             <li><a href="/profile">Profile</a></li>
             <li><a href="/discover">Discover</a></li>
-            {/* <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li> */}
+            {/* <li><a className="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li> */}
           </ul>
         </div>
       </nav>
