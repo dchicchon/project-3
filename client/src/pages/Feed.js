e42395bf76bee40cf1af6bcdd07c17ea534f86fb
Adrienne <iliu.adrienne@gmail.com>
2019-08-01 19:12:13 -0700

    add feed page

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -1,21 +1,21 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 // import Grid from "../components/Grid";
 import Post from "../components/Post";
 import SimpleContainer from "../components/Container";
 import CreatePost from "../components/CreatePost";
 
-function Feed()  {
-return (
-    <div>
-        <SimpleContainer style={{ display:'flex', justifyContent:'center' }}>
-            <br/>
-    <CreatePost/>
-            <br/>
-            <Post />
-            <Post/>
-        </SimpleContainer>
-    </div>
-)
+function Feed() {
+    return (
+        <div>
+            <SimpleContainer style={{ display: 'flex', justifyContent: 'center' }}>
+                <br />
+                <CreatePost />
+                <br />
+                <Post />
+                <Post />
+            </SimpleContainer>
+        </div>
+    )
 }
 
 export default Feed; 
