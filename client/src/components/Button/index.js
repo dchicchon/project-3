0d5e182fef211dcf1b8048f3969fe816027e34fc
blueDogs <danielchicchon@gmail.com>
2019-08-02 22:08:53 -0700

    change app.js

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.css'
 
-export default function Button({children}) {
+export default function Button({ children }) {
 
   return (
     <div>
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,65 +1,54 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Col, Row, Container } from "../components/Grid";
 import Button from '../components/Button'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class Signup extends Component {
-    state ={
-        email: ""
-    }
-
-    render() {
-        return(
-            <div>Stuff</div>
-        )
-    }
-
-}
-
-
-
-export default function Signup() {
-    const [values, setValues] = React.useState({
-        email: '',
+    state = {
+        email: "",
         password: '',
         passwordConfirm: '',
         firstName: '',
         lastName: ''
-    });
-
-    const handleChange = name => event => {
-        setValues({ ...values, [name]: event.target.value })
     }
 
-    const formSubmit = async event => {
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleFormSubmit = async event => {
         event.preventDefault();
         console.log("WE MADE IT TO THE FORM SUBMIT");
-        if (values.firstName && values.lastName && values.email && values.password && values.passwordConfirm) {
-            if (values.password === values.passwordConfirm) {
+        if (this.state.firstName && this.state.lastName && this.state.email && this.state.password && this.state.passwordConfirm) {
+            if (this.state.password === this.state.passwordConfirm) {
                 await fetch("/auth/signup", {
                     method: "POST",
                     credentials: "include",
                     mode: "cors",
                     body: JSON.stringify({
-                        password: values.password,
-                        email: values.email,
-                        firstName: values.firstName,
-                        lastName: values.lastName
+                        password: this.state.password,
+                        email: this.state.email,
+                        firstName: this.state.firstName,
+                        lastName: this.state.lastName
                     }),
                     headers: new Headers({
                         "Content-Type": "application/json"
                     })
                 })
                     .then(response => {
-                        console.log(response)
-                        console.log(response.body)
                         window.location.href = "/"
                     })
                     .catch(err => console.log(err))
 
-                setValues({
+                this.setstate({
                     email: '',
-                    password: ''
+                    password: '',
+                    passwordConfirm: '',
+                    firstName: '',
+                    lastName: ''
                 });
 
             } else {
@@ -68,142 +57,69 @@ export default function Signup() {
         } else {
             console.log("Make sure to fill out all fields")
         }
-
     }
 
-    return (
-        <div>
-            <Container>
-                <div className="card-panel center">
-                    <h1>Sign - Up</h1>
-                    <div className="row">
-                        <form className="col s12">
-                            <Row>
-                                <div className="input-field col s6">
-                                    <input id="first_name" type="text" className="validate" onChange={handleChange('first_name')} {...props} />
-                                    <label for="first_name">First Name</label>
-                                </div>
-                                <div className="input-field col s6">
-                                    <input id="last_name" type="text" className="validate" onChange={handleChange('last_name')} {...props} />
-                                    <label for="last_name">Last Name</label>
-                                </div>
-                            </Row>
-                            <Row>
-                                <div className="input-field col s12">
-                                    <input id="email" type="email" className="validate" onChange={handleChange('email')} {...props} />
-                                    <label for="email">Email</label>
-                                </div>
-                            </Row>
-
-                            <Row>
-                                <div className="input-field col s12">
-                                    <input id="password" type="password" className="validate" onChange={handleChange('password')} {...props} />
-                                    <label for="password">Password</label>
-                                </div>
-                            </Row>
-                            <Row>
-                                <div className="input-field col s12">
-                                    <input id="confirm_password" type="password" className="validate" onChange={handleChange('confirm_password')} {...props} />
-                                    <label for="password"> Confirm Password</label>
-                                </div>
-                            </Row>
-
-                            <Row>
-                                <div class="file-field input-field">
-                                    <div class="btn">
-                                        <span>Upload Photo</span>
-                                        <input type="file" onChange={handleChange('image')} {...props} />
+    render() {
+        return (
+            <div>
+                <Container>
+                    <div className="card-panel center">
+                        <h1>Sign Up</h1>
+                        <div className="row">
+                            <form className="col s12">
+                                <Row>
+                                    <div className="input-field col s6">
+                                        <input id="first_name" type="text" className="validate" onChange={this.handleInputChange} />
+                                        <label for="first_name">First Name</label>
                                     </div>
-                                    <div class="file-path-wrapper">
-                                        <input class="file-path validate" type="text" />
+                                    <div className="input-field col s6">
+                                        <input id="last_name" type="text" className="validate" onChange={this.handleInputChange} />
+                                        <label for="last_name">Last Name</label>
                                     </div>
-                                </div>
-                            </Row>
+                                </Row>
+                                <Row>
+                                    <div className="input-field col s12">
+                                        <input id="email" type="email" className="validate" onChange={this.handleInputChange} />
+                                        <label for="email">Email</label>
+                                    </div>
+                                </Row>
 
-                        </form>
+                                <Row>
+                                    <div className="input-field col s12">
+                                        <input id="password" type="password" className="validate" onChange={this.handleInputChange} />
+                                        <label for="password">Password</label>
+                                    </div>
+                                </Row>
+                                <Row>
+                                    <div className="input-field col s12">
+                                        <input id="confirm_password" type="password" className="validate" onChange={this.handleInputChange} />
+                                        <label for="password"> Confirm Password</label>
+                                    </div>
+                                </Row>
+
+                                <Row>
+                                    <div className="file-field input-field">
+                                        <div className="btn">
+                                            <span>Upload Photo</span>
+                                            <input type="file" onChange={this.handleInputChange} />
+                                        </div>
+                                        <div className="file-path-wrapper">
+                                            <input className="file-path validate" type="text" />
+                                        </div>
+                                    </div>
+                                </Row>
+
+                            </form>
+                        </div>
+                        <Button onClick={this.handleFormSubmit}>Signup</Button>
+
                     </div>
-                        <Button onClick = {this.formSubmit}>Signup</Button>
+                </Container>
+            </div >
 
-                </div>
-            </Container>
-        </div >
-    )
+        )
+    }
+
 }
 
-{/* <div className="card-panel center">
-                <h1>Signup</h1>
-                <form className={classes.container} noValidate autoComplete="off" style={{ justifyContent: "center" }}>
-
-                    <TextField style={{ width: 300 }}
-                        // id="outlined-dense"
-                        label="First Name"
-                        className={clsx(classes.textField, classes.dense)}
-                        margin="normal"
-                        variant="outlined"
-                        value={values.firstName}
-                        onChange={handleChange('firstName')}
-                    />
-                    <br />
-                    <TextField style={{ width: 300 }}
-                        id="outlined-dense"
-                        label="Last Name"
-                        className={clsx(classes.textField, classes.dense)}
-                        margin="normal"
-                        variant="outlined"
-                        value={values.lastName}
-                        onChange={handleChange("lastName")}
-                    />
-                    <br />
-                    <TextField style={{ width: 300 }}
-                        id="outlined-email-input"
-                        label="Email"
-                        className={classes.textField}
-                        type="email"
-                        name="email"
-                        autoComplete="email"
-                        margin="normal"
-                        variant="outlined"
-                        value={values.email}
-                        onChange={handleChange("email")}
-                    />
-                    <br />
-                    <TextField style={{ width: 300 }}
-                        id="outlined-password-input"
-                        label="Password"
-                        className={classes.textField}
-                        type="password"
-                        name="password"
-                        autoComplete="current-password"
-                        margin="normal"
-                        variant="outlined"
-                        value={values.password}
-                        onChange={handleChange("password")}
-                    />
-                    <br />
-                    <TextField style={{ width: 300 }}
-                        id="outlined-password-input"
-                        label="Confirm Password"
-                        className={classes.textField}
-                        type="password"
-                        autoComplete="current-password"
-                        margin="normal"
-                        variant="outlined"
-                        value={values.passwordConfirm}
-                        onChange={handleChange("passwordConfirm")}
-                    />
-                    <br />
-
-                    <SignupBtn>
-                        <div className="signup-btn" onClick={formSubmit} style={{ width: 100 }}>
-                            Signup
-                        </div>
-                    </SignupBtn>
-                    <Link to="/signup">
-                        <SignupBtn>
-                            <div className="signup-btn" style={{ width: 100 }}>
-                                Login
-                            </div>
-                        </SignupBtn>
-                    </Link>
-                </form>
-                </div> */}
+export default Signup;
