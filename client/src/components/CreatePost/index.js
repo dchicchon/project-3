737016900a20d816b976ad11c6aa2dd6a780362a
Adrienne <iliu.adrienne@gmail.com>
2019-08-02 21:17:21 -0700

    working on date component

--- client/src/components/CreatePost/index.js
+++ client/src/components/CreatePost/index.js
@@ -3,12 +3,14 @@ import CardPanel from '../CardPanel';
 import { Col, Row, Container } from "../Grid";
 import TextInput from './../TextInput/index';
 import Button from "../Button";
+import Date from "../Date";
 
 export default function CreatePost(){
     return(
         <div>
             <Container>
                 <CardPanel>
+                    <Date/>
                 <TextInput>Title</TextInput>
                 <TextInput>Record Your Journey</TextInput>
 
