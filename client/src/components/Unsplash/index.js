5535198a5cd7866863b26b84397800aa3409b8fa
Minal Kharche <minal.berkeley@gmail.com>
2019-08-06 13:08:43 -0700

    added unsplash api and got data result in json

--- client/src/components/Unsplash/index.js
+++ client/src/components/Unsplash/index.js
@@ -5,17 +5,14 @@
 // import Unsplash from 'unsplash-js';
 // import { request } from 'https';
 
-
-// // require syntax
-// const Unsplash = require('unsplash-js').default;
-
-// const unsplash = new Unsplash({
-//     applicationId: "15a3b12b4e320ca5f4b5bd3319c5e0c73401530b3953e3dc8f7352a0b5669001",
-//     secret: "594b3a846dc171579e3ce43335cfa3426d00ec2af805321f6ec8eeff36f62453"
-// });
-
-// curl 'https://api.unsplash.com/search/photos?page=1&query=birthday&client_id=15a3b12b4e320ca5f4b5bd3319c5e0c73401530b3953e3dc8f7352a0b5669001'
-
+var request = require('request');
+var apiurl = 'https://api.unsplash.com/search/photos?page=1&query=travel&client_id=key'
+request({ url: apiurl }, function (err, res) {
+    if (err) console.log(err.message);
+    else if (res) {
+        console.log(res);
+    }
+})
 
 
 
