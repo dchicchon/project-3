0c1796c9fb6a85ddef61c806a2d44fc4f2a03ff7
Minal Kharche <minal.berkeley@gmail.com>
2019-08-06 13:03:46 -0700

    create componant for unsplash api

--- client/src/components/Unsplash/index.js
+++ client/src/components/Unsplash/index.js
@@ -1,10 +1,33 @@
 //unsplash api
 
-import React from 'react';
+// import React from 'react';
+// ES Modules syntax
+// import Unsplash from 'unsplash-js';
+// import { request } from 'https';
 
-class UnsplashApi extends React.Component {
 
-    
-}
+// // require syntax
+// const Unsplash = require('unsplash-js').default;
 
-export default UnsplashApi;
+// const unsplash = new Unsplash({
+//     applicationId: "15a3b12b4e320ca5f4b5bd3319c5e0c73401530b3953e3dc8f7352a0b5669001",
+//     secret: "594b3a846dc171579e3ce43335cfa3426d00ec2af805321f6ec8eeff36f62453"
+// });
+
+// curl 'https://api.unsplash.com/search/photos?page=1&query=birthday&client_id=15a3b12b4e320ca5f4b5bd3319c5e0c73401530b3953e3dc8f7352a0b5669001'
+
+
+
+
+// class UnsplashApi extends React.Component {
+
+//     render() {
+//         return (
+//             <div>
+
+//             </div>
+//         );
+//     }
+// }
+
+// export default UnsplashApi;
