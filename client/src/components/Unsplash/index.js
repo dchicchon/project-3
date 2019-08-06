44f12f1e0e8909953bc60690880f6858153d1d8f
Minal Kharche <minal.berkeley@gmail.com>
2019-08-06 13:30:56 -0700

    added unsplash api and got json format with image url

--- client/src/components/Unsplash/index.js
+++ client/src/components/Unsplash/index.js
@@ -7,12 +7,19 @@
 
 var request = require('request');
 var apiurl = 'https://api.unsplash.com/search/photos?page=1&query=travel&client_id=key'
+
 request({ url: apiurl }, function (err, res) {
     if (err) console.log(err.message);
     else if (res) {
-        console.log(res);
+        // console.log(res);
+        var search = JSON.parse(res.body)
+        // console.log(JSON.stringify(search.results[1],null, ""));
+        search.results.forEach((e) => {
+        console.log(e.description);
+        console.log(e.urls.regular);
+    });
     }
-})
+});
 
 
 
