ebff291ad8a8cae5c70a47dd76f8a62eac0e6c53
blueDogs <danielchicchon@gmail.com>
2019-08-05 19:19:02 -0700

    remove apiKey

--- client/src/components/GoogleMap/index.js
+++ client/src/components/GoogleMap/index.js
@@ -30,7 +30,7 @@ class SimpleMap extends Component {
             // Important! Always set the container height explicitly
             <div style={{ height: '100vh', width: '100%' }}>
                 <GoogleMapReact
-                    apiKey="AIzaSyAtqm7ety_MOQiAowHsaixmgDCTFlMROwo"
+                // apiKey = (ENTER HERE)
                     defaultCenter={this.props.center}
                     defaultZoom={this.props.zoom}
                     yesIWantToUseGoogleMapApiInternals
