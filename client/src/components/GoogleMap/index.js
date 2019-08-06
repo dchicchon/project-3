27a04faa8d08ba119ad956476d57a4dd40f99515
blueDogs <danielchicchon@gmail.com>
2019-08-05 17:13:47 -0700

    made extra marker in GoogleMap component

--- client/src/components/GoogleMap/index.js
+++ client/src/components/GoogleMap/index.js
@@ -10,8 +10,8 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
 class SimpleMap extends Component {
     static defaultProps = {
         center: {
-            lat: 59.95,
-            lng: 30.33
+            lat: 37.77,
+            lng: -122.43
         },
         zoom: 11
     };
@@ -22,18 +22,23 @@ class SimpleMap extends Component {
             // Important! Always set the container height explicitly
             <div style={{ height: '100vh', width: '100%' }}>
                 <GoogleMapReact
-                    // bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
-                    apiKey='AIzaSyAtqm7ety_MOQiAowHsaixmgDCTFlMROwo '
-                    // apiKey= '478030976860-goh734t933fd9b7k8cjmbbj5u1lcgdcr.apps.googleusercontent.com'
+                    apiKey= "AIzaSyAtqm7ety_MOQiAowHsaixmgDCTFlMROwo"
                     defaultCenter={this.props.center}
                     defaultZoom={this.props.zoom}
                     yesIWantToUseGoogleMapApiInternals
                 // onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
                 >
+
                     <AnyReactComponent
-                        lat={59.955413}
-                        lng={30.337844}
-                        text="My Marker"
+                        lat={37.7897}
+                        lng={122.3973}
+                        text="Marker1"
+                    />
+
+                    <AnyReactComponent
+                    lat = {37.78}
+                    lng ={122.40}
+                    text={"Marker2"}
                     />
                 </GoogleMapReact>
             </div>
