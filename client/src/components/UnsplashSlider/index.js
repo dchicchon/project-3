ef9c591038bac05de60eb8afa2af7856fa15ec2a
Minal Kharche <minal.berkeley@gmail.com>
2019-08-07 11:30:33 -0700

    slider working

--- client/src/components/UnsplashSlider/index.js
+++ client/src/components/UnsplashSlider/index.js
@@ -1,7 +1,7 @@
 // //unsplash api
 
 // var request = require('request');
-// var apiurl = 'https://api.unsplash.com/search/photos?page=1&query=travel&client_id=15a3b12b4e320ca5f4b5bd3319c5e0c73401530b3953e3dc8f7352a0b5669001'
+// var apiurl = 'https://api.unsplash.com/search/photos?page=1&query=travel&client_id=key'
 
 // request({ url: apiurl }, function (err, res) {
 //     if (err) console.log(err.message);
