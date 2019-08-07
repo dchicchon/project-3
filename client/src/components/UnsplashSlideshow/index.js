2b0ac3e7d40d532658b0894a83afd016f9fe6fa9
Minal Kharche <minal.berkeley@gmail.com>
2019-08-07 13:54:29 -0700

    fixed card on login

--- client/src/components/UnsplashSlideshow/index.js
+++ client/src/components/UnsplashSlideshow/index.js
@@ -16,7 +16,7 @@ class UnsplashApiLogin extends Component {
     componentDidMount() {
 
         var request = require('request');
-        var apiurl = 'https://api.unsplash.com/search/photos?page=1&query=dark&client_id=15a3b12b4e320ca5f4b5bd3319c5e0c73401530b3953e3dc8f7352a0b5669001'
+        var apiurl = 'https://api.unsplash.com/search/photos?page=1&query=landscape&client_id=15a3b12b4e320ca5f4b5bd3319c5e0c73401530b3953e3dc8f7352a0b5669001'
 
         request({ url: apiurl }, (err, res) => {
             if (err) console.log(err.message);
