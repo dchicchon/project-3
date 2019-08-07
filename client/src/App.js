957ae78d7d73ebf5d44f00cf6b9c5f5576cf028c
Minal Kharche <minal.berkeley@gmail.com>
2019-08-07 11:28:36 -0700

    added UnsplashSlider component for slider background for all pages

--- client/src/App.js
+++ client/src/App.js
@@ -25,11 +25,6 @@ import image3 from './assets/bg3.jpg';
 // import image4 from './assets/bg4.jpg';
 // import image5 from './assets/bg5.jpg';
 // import image6 from './assets/bg6.jpg';
-// import image7 from './assets/bg7.jpg';
-// import image8 from './assets/bg8.jpg';
-// import image9 from './assets/bg9.jpg';
-// import image10 from './assets/bg10.jpg';
-// import image11 from './assets/bg11.jpg';
 
 
 // import UnsplashApi from "../src/components/Unsplash"
@@ -106,7 +101,7 @@ class App extends Component {
 
             /> */}
           </Switch>
-          {/* <BackgroundSlider images={[image4, image5, image6, image7, image8, image9, image10, image11]} duration={5} transition={2}/> */}
+          {/* <BackgroundSlider images={[image4, image5, image6]} duration={5} transition={2}/> */}
         </Router>
       )
     }
