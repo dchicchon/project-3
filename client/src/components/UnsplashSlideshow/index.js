18889548745fc04cd32fe7328d20fb410a84bcaa
Minal Kharche <minal.berkeley@gmail.com>
2019-08-07 15:59:53 -0700

    added unsplashapi and it's working at console

--- client/src/components/UnsplashSlideshow/index.js
+++ client/src/components/UnsplashSlideshow/index.js
@@ -19,7 +19,18 @@ class UnsplashApiLogin extends Component {
         var apiurl = 'https://api.unsplash.com/search/photos?page=1&query=landscape&client_id=15a3b12b4e320ca5f4b5bd3319c5e0c73401530b3953e3dc8f7352a0b5669001'
 
         request({ url: apiurl }, (err, res) => {
-           
+            if (err) console.log(err.message);
+            else if (res) {
+                // console.log(res);
+                var search = JSON.parse(res.body)
+                this.setState({
+                    items: search.results
+                });
+                // console.log(JSON.stringify(search.results[1], null, ""));
+                // search.results.forEach((e) => {
+                //     // console.log(e.description);
+                //     console.log(e.urls.regular);
+                // });
 
                 console.log("NEW ARRAY", search.results.map(image => image.urls.regular))
             }
