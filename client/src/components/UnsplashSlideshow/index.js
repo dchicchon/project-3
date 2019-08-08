a6a2cfb982462eb1900d34165b9d91a77735d841
Minal Kharche <minal.berkeley@gmail.com>
2019-08-07 23:07:05 -0700

    added react to implement unsplash api background slideshow

--- client/src/components/UnsplashSlideshow/index.js
+++ client/src/components/UnsplashSlideshow/index.js
@@ -1,19 +1,57 @@
 // //unsplash api slideshow for login and signup
 
+
+import React, { Component } from 'react';
+import BackgroundSlideshow from 'react-background-slideshow';
+
+class UnsplashApiLogin extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: []
+        }
+    }
+
+    componentDidMount() {
+
+        var request = require('request');
+        var apiurl = 'https://api.unsplash.com/search/photos?page=1&query=landscape&client_id=key'
+
+        request({ url: apiurl }, (err, res) => {
+            if (err) console.log(err.message);
+            else if (res) {
+               
+            }
+        });
+
+    }
+
+    render() {
+       
+    }
+}
+
+export default UnsplashApiLogin;
+
+
+
+
+
 //---------trial without react------------
 
-var request = require('request');
-var apiurl = 'https://api.unsplash.com/search/photos?page=1&query=travel&client_id=key'
+// var request = require('request');
+// var apiurl = 'https://api.unsplash.com/search/photos?page=1&query=travel&client_id=key'
 
-request({ url: apiurl }, function (err, res) {
-    if (err) console.log(err.message);
-    else if (res) {
-        // console.log(res);
-        var search = JSON.parse(res.body)
-        console.log(JSON.stringify(search.results[1],null, ""));
-        search.results.forEach((e) => {
-        console.log(e.description);
-        console.log(e.urls.regular);
-    });
-    }
-});
+// request({ url: apiurl }, function (err, res) {
+//     if (err) console.log(err.message);
+//     else if (res) {
+//         // console.log(res);
+//         var search = JSON.parse(res.body)
+//         console.log(JSON.stringify(search.results[1],null, ""));
+//         search.results.forEach((e) => {
+//         console.log(e.description);
+//         console.log(e.urls.regular);
+//     });
+//     }
+// });
