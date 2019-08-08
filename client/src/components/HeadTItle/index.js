a064a9294b0959757a8490fe42d2400e014fff34
Adrienne <iliu.adrienne@gmail.com>
2019-08-07 23:47:15 -0700

    center title

--- client/src/components/HeadTItle/index.js
+++ client/src/components/HeadTItle/index.js
@@ -4,14 +4,28 @@ import { Col, Row, Container } from "../Grid"
 
 function HeadTitle(props) {
     return (
-        <wrapper>
-                        <div className="input-field col s4" style={{textAlign: "center"}}>
+
+        <div>
+           
+        <Container>
+            <Row>
+                <Col size="s4"> </Col>
+
+                <div className="row center">
+                    <div className="input-field col s4">
+        {/* // <wrapper>
+        //                 <div className="input-field col s4" style={{textAlign: "center"}}> */}
                             {/* <i className="material-icons prefix">textsms</i> */}
                             {/* <input type="text" id="autocomplete-input" className="autocomplete" /> */}
                             {/* <label for="autocomplete-input">Discover</label> */}
                             <h3 className="grey-text text-darken-3" >{props.children}</h3>
                         </div>
-        </wrapper>
+                        </div>
+        {/* // </wrapper> */}
+        <Col size="s4"> </Col>
+                </Row>
+            </Container>
+        </div>
 
     );
 
